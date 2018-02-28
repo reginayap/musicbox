2b3789996add5fa84c7dddf3eeea0a1a7a1fd916
reginayap <35645652+reginayap@users.noreply.github.com>
2018-02-28 13:53:19 -0500

    Finished the Index, added images

--- index.php
+++ index.php
@@ -25,14 +25,12 @@
 						<!-- Featured Post -->
 							<article class="post featured">
 								<header class="major">
-									<span class="date">April 25, 2017</span>
-									<h2><a href="#">And this is a<br />
-									massive headline</a></h2>
-									<p>Aenean ornare velit lacus varius enim ullamcorper proin aliquam<br />
-									facilisis ante sed etiam magna interdum congue. Lorem ipsum dolor<br />
-									amet nullam sed etiam veroeros.</p>
+									<span class="date">February 28, 2018</span>
+									<h2><a href="#">Catch These<br />
+									Sweet tunes</a></h2>
+									<p>Midterms got you down? Take a break and listen to our music to relax and rewind.</p>
 								</header>
-								<a href="#" class="image main"><img src="images/pic01.jpg" alt="" /></a>
+								<a href="#" class="image main"><img src="images/shootingstar.gif" alt="" /></a>
 								<ul class="actions">
 									<li><a href="#" class="button big">Full Story</a></li>
 								</ul>
@@ -87,30 +85,6 @@
 										<li><a href="#" class="button">Full Story</a></li>
 									</ul>
 								</article>
-								<article>
-									<header>
-										<span class="date">April 11, 2017</span>
-										<h2><a href="#">Odio magna<br />
-										sed consectetur</a></h2>
-									</header>
-									<a href="#" class="image fit"><img src="images/pic06.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
-								</article>
-								<article>
-									<header>
-										<span class="date">April 7, 2017</span>
-										<h2><a href="#">Augue lorem<br />
-										primis vestibulum</a></h2>
-									</header>
-									<a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
-								</article>
 							</section>
 
 						<!-- Footer -->
@@ -118,13 +92,13 @@
 								<div class="pagination">
 									<!--<a href="#" class="previous">Prev</a>-->
 									<a href="#" class="page active">1</a>
-									<a href="#" class="page">2</a>
+									<!--<a href="#" class="page">2</a>
 									<a href="#" class="page">3</a>
 									<span class="extra">&hellip;</span>
 									<a href="#" class="page">8</a>
 									<a href="#" class="page">9</a>
 									<a href="#" class="page">10</a>
-									<a href="#" class="next">Next</a>
+									<a href="#" class="next">Next</a>-->
 								</div>
 							</footer>
 
@@ -154,16 +128,23 @@
 						<section class="split contact">
 							<section class="alt">
 								<h3>Address</h3>
-								<p>1234 Somewhere Road #87257<br />
-								Nashville, TN 00000-0000</p>
+								<p>
+									University of Virginia<br />
+									P.O. Box 400133<br />
+									103 Monroe Hall - 248 McCormick Road<br />
+									Charlottesville, VA 22904<br />
+								</p>
 							</section>
 							<section>
 								<h3>Phone</h3>
-								<p><a href="#">(000) 000-0000</a></p>
+								<p>
+									(434) 101- 0011
+								</p>
 							</section>
 							<section>
 								<h3>Email</h3>
-								<p><a href="#">info@untitled.tld</a></p>
+								<p><a href="#">hs4ah@virginia.edu</a></p>
+								<p><a href="#">ry3zg@virginia.edu</a></p>
 							</section>
 							<section>
 								<h3>Social</h3>
@@ -179,7 +160,7 @@
 
 				<!-- Copyright -->
 					<div id="copyright">
-						<ul><li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
+						<ul><li>Musicbox</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
 					</div>
 
 			</div>
