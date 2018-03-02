6eb3c3581f16e40ede1ed560b10829be630db224
hsau88 <hs4ah@virginia.edu>
2018-03-01 23:00:19 -0500

    hiu's page
    Changed header and description to test if changes worked on my page

--- generic.php
+++ generic.php
@@ -26,11 +26,10 @@
 							<section class="post">
 								<header class="major">
 									<span class="date">April 25, 2017</span>
-									<h1>This is a<br />
-									Generic Page</h1>
-									<p>Aenean ornare velit lacus varius enim ullamcorper proin aliquam<br />
-									facilisis ante sed etiam magna interdum congue. Lorem ipsum dolor<br />
-									amet nullam sed etiam veroeros.</p>
+									<h1>Hi! Welcome To My Page!</h1>
+									<p> My name is Hiu Hiu, and I am a third year Computer Science and Music Double Major. <br /> Although I am
+                                        a city girl at heart (woo 852!), I have learnt to love the beauty around Charlottesville. <br />
+                                        Wanna know what motivates me? Fuels me? Fulfills me? Keep scrolling!</p>
 								</header>
 								<div class="image main"><img src="images/pic01.jpg" alt="" /></div>
 								<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sapien risus, commodo eget turpis at, elementum convallis enim turpis, lorem ipsum dolor sit amet nullam.</p>
