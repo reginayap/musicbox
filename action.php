1e3a9adbe5a56c1a49f35fcdeffd743c9b2becab
reginayap <35645652+reginayap@users.noreply.github.com>
2018-03-15 13:27:56 -0400

    header and footer are included with php function

--- action.php
+++ action.php
@@ -5,160 +5,7 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html>
-<<<<<<< HEAD
-  <head>
-    <title>Music Box</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="stylesheet" href="assets/css/main.css" />
-    <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
-    </head>
-  <body class="is-loading">
-
-    <!-- Wrapper -->
-    <div id="wrapper">
-
-      <style>
-
-#nav {
-font-size: 14 px;
-}
-
-</style>
-
-<!-- Intro -->
-<div id="intro">
-  <h1>Welcome to<br />
-    Music Box</h1>
-  <p>A free collection of Hiu Hiu Sau's and Regina Yap's Favorite Tracks</p>
-  <ul class="actions">
-    <li><a href="#header" class="button icon solo fa-play-circle scrolly">Continue</a></li>
-    </ul>
-  </div>
-
-<!-- Header -->
-<header id="header">
-  <a href="index.php" class="logo">Music Box</a>
-  </header>
-
-<!-- Nav -->
-<nav id="nav">
-  <ul class="links">
-    <li><a href="index.php">Welcome to Music Box</a></li>
-    <li><a href="hiuhiu.php">Hiu Hiu's Tunes</a></li>
-    <li><a href="regina.php">Regina's Tunes</a></li>
-    <li><a href="our_music.php">Our Music</a></li>
-    <li><a href="our_music.php">Reflections</a></li>
-    <li><a href="sign_up.php">Sign Up</a></li>
-    <!--<li><a href="elements.html">Elements Reference</a></li>-->
-    </ul>
-  <ul class="icons">
-    <!--<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-	<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-	<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-	<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>-->
-    <li><a href="#" class="icon fa-user-circle"><span class="label">Login</span></a></li>
-    </ul>
-  </nav>
-
-<!-- Main -->
-<div id="main">
-
-  <!-- Post -->
-  <section class="post">
-    <header class="major">
-      <h3>Your message has been sent successfully.</h3>
-
-      <!-- Post -->
-      <p style="color:black;font-size:16px;line-height:1.6">
-	<?php
-	   echo "Hi ";
-	   echo ($_POST['name']);
-	   echo "!";
-	   echo "<br>";
-	   echo "Thanks for taking your time to look through Music Box, and for sending us a message! ";
-	   echo "<br>";
-	   echo "We will get to your message soon and send a reply back to ";
-	   echo strtolower($_POST['email']);
-	   echo ".";
-	   ?>
-      </p> 
-     </header>
-    </br>
-  </section>
-</div>
-
-<!-- Footer -->
-<style>
-
-#footer {
-    font-size: 16 px;
-}
-
-</style>
-
-
-<footer id="footer">
-  <section>
-    <form method="post" action="action.php">
-      <div class="field">
-	<label for="name">Name</label>
-	<input type="text" name="name" id="name" />
-	</div>
-      <div class="field">
-	<label for="email">Email</label>
-	<input type="text" name="email" id="email" />
-	</div>
-      <div class="field">
-	<label for="message">Message</label>
-	<textarea name="message" id="message" rows="3"></textarea>
-	</div>
-      <ul class="actions">
-	<li><input type="submit" value="Send Message" /></li>
-	</ul>
-      </form>
-    </section>
-  <section class="split contact">
-    <section class="alt">
-      <h3>Address</h3>
-      <p>
-	University of Virginia<br />
-	P.O. Box 400133<br />
-	103 Monroe Hall - 248 McCormick Road<br />
-	Charlottesville, VA 22904<br />
-	</p>
-      </section>
-    <section>
-      <h3>Phone</h3>
-      <p>
-	(434) 101- 0011
-	</p>
-      </section>
-    <section>
-      <h3>Email</h3>
-      <p>Hiu Hiu: <a href="#">hs4ah@virginia.edu</a><br/>Regina: <a href="#">ry3zg@virginia.edu</a></p>
-      </section>
-    <section>
-      <h3>Social</h3>
-      <ul class="icons alt">
-	<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
-	<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
-	<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
-	<li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
-	</ul>
-      </section>
-    </section>
-  </footer>
-
-<!-- Copyright -->
-<div id="copyright">
-  <ul><li>Musicbox</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
-  </div>
-
-</div>
-=======
 	<head>
-    // this is my first comment to regina! 
 		<title>Music Box</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
@@ -175,16 +22,28 @@ font-size: 14 px;
 				<!-- Main -->
 					<div id="main">
 
-						<section align="center">
+						<!-- Post -->
+						  <section class="post">
+							<header class="major">
+							  <h3>Your message has been sent successfully.</h3>
+
+							  <!-- Post -->
+							  <p style="color:black;font-size:16px;line-height:1.6">
 							<?php
-							echo "Hi";
-							echo "Hi!";
-							echo ($_POST['name']);
-							echo "Thanks for taking your time to look through Music Box, and for sending us a message! ";
-							echo "We will get to your message soon and send you a reply at ";
-							echo strtolower($_POST['email']);
-							?>
-						</section>
+							   echo "Hi ";
+							   echo ($_POST['name']);
+							   echo "!";
+							   echo "<br>";
+							   echo "Thanks for taking your time to look through Music Box, and for sending us a message! ";
+							   echo "<br>";
+							   echo "We will get to your message soon and send a reply back to ";
+							   echo strtolower($_POST['email']);
+							   echo ".";
+							   ?>
+							  </p> 
+							 </header>
+							</br>
+						  </section>
 					</div>
 
 				<!-- Footer -->
