9f295660d5c8d04d6af6db19cfb84e3320156e8e
hsau88 <hs4ah@virginia.edu>
2018-03-15 12:22:20 -0400

    action

--- action.php
+++ action.php
@@ -5,14 +5,13 @@
 <body>
 <?php
     echo "Hi";
-    echo htmlspecialchars($_POST['name']) <br />;
-    echo "Thanks for taking your time to look through Music Box, and for sending us a message!" <br />;
+    echo "Hi!";
+    echo ($_POST['name']);
+    echo "Thanks for taking your time to look through Music Box, and for sending us a message! ";
     echo "We will get to your message soon and send you a reply at ";
-    echo htmlspecialchars ($_POST['email']);
-    echo " once we can!" <br />;
-?>
-
-</body>
+    echo strtolower($_POST['email']);
+    ?>
+    
+    </body>
 </html>
-
 
