878ac5905f5f68e7b7e95695c2d6d66a3647b9c9
reginayap <35645652+reginayap@users.noreply.github.com>
2018-03-15 12:31:00 -0400

    Formatted the action.php page to match the rest of our website

--- action.php
+++ action.php
@@ -1,17 +1,54 @@
+<!DOCTYPE HTML>
+<!--
+	Massively by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
 <html>
-<head>
+	<head>
+    // this is my first comment to regina! 
+		<title>Music Box</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-loading">
 
-</head>
-<body>
-<?php
-    echo "Hi";
-    echo "Hi!";
-    echo ($_POST['name']);
-    echo "Thanks for taking your time to look through Music Box, and for sending us a message! ";
-    echo "We will get to your message soon and send you a reply at ";
-    echo strtolower($_POST['email']);
-    ?>
-    
-    </body>
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<?php include("header.php");?>
+
+				<!-- Main -->
+					<div id="main">
+
+						<section>
+							<?php
+							echo "Hi";
+							echo "Hi!";
+							echo ($_POST['name']);
+							echo "Thanks for taking your time to look through Music Box, and for sending us a message! ";
+							echo "We will get to your message soon and send you a reply at ";
+							echo strtolower($_POST['email']);
+							?>
+						</section>
+					</div>
+
+				<!-- Footer -->
+					<?php include("footer.php");?>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
 </html>
+
 
